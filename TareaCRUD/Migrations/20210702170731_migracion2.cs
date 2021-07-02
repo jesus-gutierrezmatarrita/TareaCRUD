663a5310d75dac31b06bcad66165ed416d53b776
jesus <jesusantonio.gutierrezmatarrita@gmail.com>
2021-07-02 11:30:12 -0600

    Added a new field to comply with the "at least" 4 fields requirement. Fixed some bugs. Ready to deliver

---
+++ TareaCRUD/Migrations/20210702170731_migracion2.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TareaCRUD.Migrations
+{
+    public partial class migracion2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CourseId",
+                table: "Course",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Modalidad",
+                table: "Course",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Course_CourseId",
+                table: "Course",
+                column: "CourseId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Course_Course_CourseId",
+                table: "Course",
+                column: "CourseId",
+                principalTable: "Course",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Course_Course_CourseId",
+                table: "Course");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Course_CourseId",
+                table: "Course");
+
+            migrationBuilder.DropColumn(
+                name: "CourseId",
+                table: "Course");
+
+            migrationBuilder.DropColumn(
+                name: "Modalidad",
+                table: "Course");
+        }
+    }
+}
